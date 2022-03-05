bbf36a120db1ccf8b60a1e72c776530934efd5e7
emmzak <emmanuelzakaryan@gmail.com>
2022-03-05 23:23:24 +0100

    changed KO message template

--- app/src/main/java/com/app/app/config/SMSTemplate.kt
+++ app/src/main/java/com/app/app/config/SMSTemplate.kt
@@ -2,7 +2,7 @@ package com.app.app.config
 
 enum class SMSTemplate(val text: String) {
     OK("Salut, tout va bien"),
-    KO("Salut, j'ai un souci"),
+    KO("Salut, j'ai un souci, appelle-moi dès que tu peux"),
     ALERT("Alerte SOS de %s %s"),
     SOS("Num Sécu: %s, Médecin-%s, Localisation-%s"),
     LOCATION("Localisation {}")
